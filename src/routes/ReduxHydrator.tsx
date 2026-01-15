0252539b364ee9a91bccc9787b86a1d1552b93bc
arjun <arjunjhukal.makura@gmail.com>
2026-01-15 11:46:53 +0545

    fixed the lining git push

--- src/routes/ReduxHydrator.tsx
+++ src/routes/ReduxHydrator.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-import { useAppDispatch } from "@/hooks/hook";
-import { setTokens } from "@/slice/authSlice";
 
 export default function ReduxHydrator({ token }: { token: string; }) {
-    const dispatch = useAppDispatch();
 
     // useEffect(() => {
     //     dispatch(setTokens({ access_token: token, user }));
