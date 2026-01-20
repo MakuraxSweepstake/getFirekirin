777f9dedc58fcc21b8b4f47da50717a1c879f74a
arjun <arjunjhukal.makura@gmail.com>
2026-01-20 15:20:33 +0545

    updated the detail page

--- src/app/(dashboard)/(user)/(outsideAuth)/exclusive-games/[id]/page.tsx
+++ src/app/(dashboard)/(user)/(outsideAuth)/exclusive-games/[id]/page.tsx
@@ -1,56 +1,37 @@
 import ExclusiveGameDetail from "@/components/pages/dashboard/userDashboard/games/exclusiveGames/exclusiveGameDetail";
 import { getSingleGame } from "@/serverApi/game";
 import { Metadata } from "next";
-import { notFound } from "next/navigation";
 
 const SITE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL!;
 
-type PageProps = {
-    params: { id: string };
-};
+export async function generateMetadata(props: { params: Promise<{ id: string }> }): Promise<Metadata> {
+    const { id } = await props.params;
+    const canonicalUrl = `${SITE_URL}/exclusive-games/${id}`.replace(/\/$/, "");
+    const game = await getSingleGame(id);
 
-export async function generateMetadata(
-    { params }: PageProps
-): Promise<Metadata> {
-    try {
-        const { id } = params;
-        const canonicalUrl = `${SITE_URL}/exclusive-games/${id}`.replace(/\/$/, "");
-        const game = await getSingleGame(id);
-
-        if (!game?.data) {
-            return {};
-        }
-
-        return {
-            title: game.data.meta?.meta_title || game.data.name,
-            description: game.data.meta?.meta_description || game.data.name,
-            openGraph: {
-                title: game.data.meta?.meta_title || game.data.name,
-                description: game.data.meta?.meta_description || game.data.name,
-                images: game.data.meta?.og_image_url,
-            },
-            twitter: {
-                title: game.data.meta?.meta_title || game.data.name,
-                description: game.data.meta?.meta_description || game.data.name,
-                images: game.data.meta?.og_image_url,
-            },
-            alternates: {
-                canonical: canonicalUrl,
-            },
-        };
-    } catch {
-        return {};
-    }
+    return {
+        title: game?.data?.meta?.meta_title || game?.data?.name,
+        description: game?.data?.meta?.meta_description || game?.data?.name,
+        openGraph: {
+            title: game?.data?.meta?.meta_title || game?.data?.name,
+            description: game?.data?.meta?.meta_description || game?.data?.name,
+            images: game?.data?.meta?.og_image_url
+        },
+        twitter: {
+            title: game?.data?.meta?.meta_title || game?.data?.name,
+            description: game?.data?.meta?.meta_description || game?.data?.name,
+            images: game?.data?.meta?.og_image_url
+        },
+        alternates: {
+            canonical: canonicalUrl,
+        },
+    };
 }
 
 export default async function UserGameDetail(props: { params: Promise<{ id: string }> }) {
     const { id } = await props.params;
 
     const game = await getSingleGame(id);
-
-    if (!game?.data) {
-        notFound();
-    }
 
     return <ExclusiveGameDetail game={game} />;
 }
