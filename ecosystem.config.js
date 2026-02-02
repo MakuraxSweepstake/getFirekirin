d49f99e281a51203988dc9d8d2bf5e8ed833bbb6
arjun <arjunjhukal.makura@gmail.com>
2026-02-02 12:22:50 +0545

    updated the firekirin new deployment pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
     apps: [{
         name: 'getFirekirin',
         script: 'node_modules/next/dist/bin/next',
-        args: 'start -p 3000',
-        cwd: '/var/www/getFirekirin',
+        args: 'start',
+        cwd: '/home/1522942.cloudwaysapps.com/swsjapwyjk',
         instances: 1,
+        exec_mode: 'cluster',
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
@@ -12,8 +13,11 @@ module.exports = {
             NODE_ENV: 'production',
             PORT: 3000
         },
-        error_file: '/var/www/getFirekirin/logs/pm2-error.log',
-        out_file: '/var/www/getFirekirin/logs/pm2-out.log',
-        log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+        error_file: '/home/1522942.cloudwaysapps.com/swsjapwyjk/logs/pm2-error.log',
+        out_file: '/home/1522942.cloudwaysapps.com/swsjapwyjk/logs/pm2-out.log',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        merge_logs: true,
+        max_restarts: 10,
+        min_uptime: '10s'
     }]
 }
