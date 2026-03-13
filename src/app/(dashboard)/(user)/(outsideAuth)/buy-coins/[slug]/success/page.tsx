8cb8d25932d662881d37e02e2f01827083dd6f50
arjun <arjunjhukal.makura@gmail.com>
2026-03-13 22:26:19 +0545

    final

--- src/app/(dashboard)/(user)/(outsideAuth)/buy-coins/[slug]/success/page.tsx
+++ src/app/(dashboard)/(user)/(outsideAuth)/buy-coins/[slug]/success/page.tsx
@@ -4,7 +4,6 @@
 
 import GlassWrapper from '@/components/molecules/GlassWrapper'
 import { useAppDispatch } from '@/hooks/hook'
-import Private from '@/routes/Private'
 import { setTokens } from '@/slice/authSlice'
 import { restoreAuthFromCookies } from '@/utils/authSession'
 import Image from 'next/image'
@@ -52,27 +51,25 @@ export default function PaymentSuccess() {
     }
 
     return (
-       <Private>
-            <GlassWrapper className="max-w-[520px] mx-auto flex flex-col gap-3 items-center text-center p-6">
-                <Image
-                    src="/assets/images/verify-email.png"
-                    alt="Payment Success"
-                    width={180}
-                    height={140}
-                />
-                <h1 className="text-[24px] lg:text-[32px] leading-[120%] font-bold mb-4 text-green-500">
-                    Payment Successful
-                </h1>
-                <p className="text-[14px] leading-[150%] font-normal lg:text-[16px] mb-4">
-                    Your payment was processed successfully.
-                </p>
-                <Link
-                    href={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/exclusive-games/${slug}`}
-                    className="ss-btn bg-primary-grad"
-                >
-                    View Game Detail
-                </Link>
-            </GlassWrapper>
-       </Private>
+        <GlassWrapper className="max-w-[520px] mx-auto flex flex-col gap-3 items-center text-center p-6">
+            <Image
+                src="/assets/images/verify-email.png"
+                alt="Payment Success"
+                width={180}
+                height={140}
+            />
+            <h1 className="text-[24px] lg:text-[32px] leading-[120%] font-bold mb-4 text-green-500">
+                Payment Successful
+            </h1>
+            <p className="text-[14px] leading-[150%] font-normal lg:text-[16px] mb-4">
+                Your payment was processed successfully.
+            </p>
+            <Link
+                href={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/exclusive-games/${slug}`}
+                className="ss-btn bg-primary-grad"
+            >
+                View Game Detail
+            </Link>
+        </GlassWrapper>
     )
 }
