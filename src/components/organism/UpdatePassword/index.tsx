651897899ea3bcfb5289bc79df7a2043295e2f31
arjun <arjunjhukal.makura@gmail.com>
2026-03-19 15:29:10 +0545

    minor chagnes

--- src/components/organism/UpdatePassword/index.tsx
+++ src/components/organism/UpdatePassword/index.tsx
@@ -35,7 +35,7 @@ export default function UpdatePassword() {
                 }).unwrap();
                 dispatch(
                     showToast({
-                        variant: ToastVariant.ERROR,
+                        variant: ToastVariant.SU,
                         message: response.message || "Something went wrong"
                     })
                 )
